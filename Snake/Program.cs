0f4c07b191c10beeb47e37ff301b96821d786d3c
dm <semu-dima@yandex.ru>
2021-02-04 20:58:55 +0300

    Add ctor Point

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,16 +6,10 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-			var p1 = new Point();
-			p1.x = 1;
-			p1.y = 3;
-			p1.sym = '*';
+			var p1 = new Point(1, 3, '*');
 			p1.Draw();
 
-			var p2 = new Point();
-			p2.x = 4;
-			p2.y = 5;
-			p2.sym = '#';
+			var p2 = new Point(4, 5, '#');
 			p2.Draw();
 		}
 	}

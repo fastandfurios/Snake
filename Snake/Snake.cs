9286438ca47e16e7d5a71e7074486ee70a880293
dm <semu-dima@yandex.ru>
2021-02-06 22:48:10 +0300

    change class Snake and Point

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -8,8 +8,11 @@ namespace Snake
 {
     public class Snake : Figure
     {
+	    private Direction _direction;
+
 	    public Snake(Point tail, int length, Direction direction)
 	    {
+		    _direction = direction;
 		    _pointsList = new List<Point>();
 		    for (int i = 0; i < length; i++)
 		    {
@@ -17,6 +20,25 @@ namespace Snake
 			    point.Move(i, direction);
 			    _pointsList.Add(point);
 		    }
+	    }
+
+	    internal void Move()
+	    {
+		    var tail = _pointsList.First();
+		    _pointsList.Remove(tail);
+		    var head = GetNextPoint();
+			_pointsList.Add(head);
+
+			tail.Clear();
+			head.Draw();
+	    }
+
+	    private Point GetNextPoint()
+	    {
+		    var head = _pointsList.Last();
+		    var nextPoint = new Point(head);
+			nextPoint.Move(1, _direction);
+			return nextPoint;
 	    }
     }
 }
